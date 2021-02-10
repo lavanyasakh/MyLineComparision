cb3ecc0956f8000111a01ad5c9600837e556cfd7
Lavanya <lavanyace929@gmail.com>
2021-02-10 19:43:48 +0530

    Refactor:Checked If The Two Lines Are Equal

--- LineComp.java
+++ LineComp.java
@@ -4,29 +4,29 @@ public class LineComp {
 	public static void main(String[] args) {
 		Scanner data = new Scanner(System.in);
 		System.out.println("Enter x1 and Y1 values of First point");
-		int X1 = data.nextInt();
-		int Y1 = data.nextInt();
+		int x1 = data.nextInt();
+		int y1 = data.nextInt();
 		System.out.println("Enter X2 and Y2 of Second Point");
-		int X2 = data.nextInt();
-		int Y2 = data.nextInt();
-		int X = X2 - X1;
-		int Y = Y2 - Y1;
-		double A = Math.pow(X, 2) + Math.pow(Y,2);
-		double S = Math.sqrt(A);
-		System.out.println("The Length of The Line is =" + S);
+		int x2 = data.nextInt();
+		int y2 = data.nextInt();
+		int x = x2 - x1;
+		int y = y2 - y1;
+		double sum = Math.pow(x, 2) + Math.pow(y,2);
+		double length = Math.sqrt(sum);
+		System.out.println("The Length of The Line is =" + length);
 
 		System.out.println("Enter X3 And Y3 Values Of Second Line");
-		int X3 = data.nextInt();
-		int Y3 = data.nextInt();
+		int x3 = data.nextInt();
+		int y3 = data.nextInt();
 		System.out.println("Enter X4 And Y4 Values of Second Line");
-		int X4 = data.nextInt();
-		int Y4 = data.nextInt();
-		int X11 = X4 - X3;
-		int Y11 = Y4 - Y3;
-		double A1 = Math.pow(X11, 2) + Math.pow(Y11, 2);
-		double S1 = Math.sqrt(A1);
-		System.out.println("The Length of Line is =" + S1);
-		if (S == S1) 
+		int x4 = data.nextInt();
+		int y4 = data.nextInt();
+		int x11 = x4 - x3;
+		int y11 = y4 - y3;
+		double sum1 = Math.pow(x11, 2) + Math.pow(y11, 2);
+		double length1 = Math.sqrt(sum1);
+		System.out.println("The Length of Line is =" + length1);
+		if (length == length1) 
 			System.out.println("The Lines Are Equal");
 		else 
 			System.out.println("The Lines Are Not Equal");

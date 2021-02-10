1c5f3ee3df6db9b9a0a6c8758e190a9d8db20d90
Lavanya <lavanyace929@gmail.com>
2021-02-10 19:37:40 +0530

    Refactor:The Length Of Line is Calculated

--- LineComp.java
+++ LineComp.java
@@ -4,15 +4,15 @@ public class LineComp {
 	public static void main(String[] args) {
 		Scanner data = new Scanner(System.in);
 		System.out.println("Enter x1 and Y1 values of First point");
-		int X1 = data.nextInt();
-		int Y1 = data.nextInt();
+		int x1 = data.nextInt();
+		int y1 = data.nextInt();
 		System.out.println("Enter X2 and Y2 of Second Point");
-		int X2 = data.nextInt();
-		int Y2 = data.nextInt();
-		int X = X2 - X1;
-		int Y = Y2 - Y1;
-		double A = Math.pow(X, 2) + Math.pow(Y,2);
-		double S = Math.sqrt(A);
-		System.out.println("The Length of The Line is =" + S);
+		int x2 = data.nextInt();
+		int y2 = data.nextInt();
+		int x = x2 - x1;
+		int y = y2 - y1;
+		double sum = Math.pow(x, 2) + Math.pow(y,2);
+		double length = Math.sqrt(sum);
+		System.out.println("The Length of The Line is =" + length);
 	}
 }

ce56056e1cd72f9128a5c23be360500229acd92c
Lavanya <lavanyace929@gmail.com>
2021-02-11 14:24:32 +0530

    Refactor:The Two Lines Are Compared Using Objects

--- LineComp.java
+++ LineComp.java
@@ -1,28 +1,40 @@
 import java.util.*;
 public class LineComp {
+	private final int x1;
+	private final int x2;
+	private final int y1;
+	private final int y2;
+	private final int x3;
+	private final int x4;
+	private final int y3;
+	private final int y4;
+
+	public LineComp(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.x3 = x3;
+		this.x4 = x4;
+		this.y3 = y3;
+		this.y4 = y4;
+	}
 
 	public static void main(String[] args) {
-		Scanner data = new Scanner(System.in);
-		System.out.println("Enter x1 and Y1 values of First point");
-		int x1 = data.nextInt();
-		int y1 = data.nextInt();
-		System.out.println("Enter X2 and Y2 of Second Point");
-		int x2 = data.nextInt();
-		int y2 = data.nextInt();
-		int x = x2 - x1;
-		int y = y2 - y1;
+		LineComp line1 = new LineComp(25, 45, 67, 89, 34, 56, 26, 48);
+		System.out.println("Entered x1 and Y1 values of First point");
+		System.out.println("Entered X2 and Y2 of Second Point");
+		int x = line1.x2 - line1.x1;
+		int y = line1.y2 - line1.y1;
 		double sum = Math.pow(x, 2) + Math.pow(y,2);
 		double length = Math.sqrt(sum);
 		System.out.println("The Length of The Line is =" + length);
 
-		System.out.println("Enter X3 And Y3 Values Of Second Line");
-		int x3 = data.nextInt();
-		int y3 = data.nextInt();
-		System.out.println("Enter X4 And Y4 Values of Second Line");
-		int x4 = data.nextInt();
-		int y4 = data.nextInt();
-		int x11 = x4 - x3;
-		int y11 = y4 - y3;
+
+		System.out.println("Entered X3 And Y3 Values Of Second Line");
+		System.out.println("Entered X4 And Y4 Values of Second Line");
+		int x11 = line1.x4 - line1.x3;
+		int y11 = line1.y4 - line1.y3;
 		double sum1 = Math.pow(x11, 2) + Math.pow(y11, 2);
 		double length1 = Math.sqrt(sum1);
 		System.out.println("The Length of Line is =" + length1);
